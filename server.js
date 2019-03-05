100071d00d07964b0f0c852ac20adf151eabb018
mh4722 <hassan.motivala@gmail.com>
2019-03-04 19:47:50 -0500

    Registering a new user and login code base set up. The system uses JWT for authentication.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const rally = require('./routes/api/rally');
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//DB COnfig
+const db = require('./config/keys').mongoURI;
+
+//Connect to DB
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongodb connected."))
+  .catch(err => console.log(err));
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Use routes 
+app.use('/api/users', users);
+app.use('/api/rally', rally);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
