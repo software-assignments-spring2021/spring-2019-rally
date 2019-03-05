e6b30af66984769a5285432ac86957ad136969ed
Christy Welch <christywelch2@Christys-MacBook-Pro-2.local>
2019-03-05 01:56:55 -0500

    keys.js update

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://rallyDevs:agileftw@testingcluster-shard-00-00-78gbf.mongodb.net:27017,testingcluster-shard-00-01-78gbf.mongodb.net:27017,testingcluster-shard-00-02-78gbf.mongodb.net:27017/test?ssl=true&replicaSet=testingCluster-shard-0&authSource=admin&retryWrites=true',
+    secretOrKey: "secret"
+};
