2237732501688d859d4f63b6755d5cba4affe359
Christy Welch <christywelch2@Christys-MacBook-Pro-2.local>
2019-03-14 04:34:09 -0400

    create registration UI & link to DB

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
+import { withRouter } from 'react-router-dom';
+
+class Register extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      name:'',
+      email:'',
+      username:'',
+      password:'',
+      password2:'',
+      errors: {}
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.errors){
+      this.setState({errors: nextProps.errors});
+    }
+  }
+
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+
+    this.props.registerUser(newUser, this.props.history);
+
+  }
+
+  render() {
+
+    const { errors } = this.state;
+
+    return(
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your Rally account
+              </p>
+              <form noValidate onSubmit = {this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
+                    placeholder="Name"
+                    name="name"
+                    value = {this.state.name}
+                    onChange = {this.onChange}
+                  />
+                  {errors.name && (<div className='invalid-feedback'>{errors.name}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value = {this.state.email}
+                    onChange = {this.onChange}
+                  />
+                  {errors.email && (<div className='invalid-feedback'>{errors.email}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="username"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.username
+                    })}
+                    placeholder="Username"
+                    name="username"
+                    value = {this.state.username}
+                    onChange = {this.onChange}
+                  />
+                  {errors.username && (<div className='invalid-feedback'>{errors.username}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value = {this.state.password}
+                    onChange = {this.onChange}
+                  />
+                  {errors.password && (<div className='invalid-feedback'>{errors.password}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value = {this.state.password2}
+                    onChange = {this.onChange}
+                  />
+                  {errors.password2 && (<div className='invalid-feedback'>{errors.password2}</div>)}
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+
+}
+
+// remember auth comes from rootReducer
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
