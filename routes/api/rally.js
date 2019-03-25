e1f51660a8f04de7d6dc791e709b263f9813c398
nanako-chung <nanako.s.chung@gmail.com>
2019-03-25 19:32:49 -0400

    Fixed previous error; unit test not working and to be done

--- routes/api/rally.js
+++ routes/api/rally.js
@@ -18,66 +18,66 @@ const User = require('../../models/User');
 // @access   Public
 router.get('/test', (req, res) => res.json({msg: "Rally  works"}));
 
-// @route    GET api/rally/create
+// @route    GET api/rally/register
 // @desc     Create rally
 // @access   Public
 router.post('/register', (req, res) => {
 	//Validation
-    const { errors, isValid } = validateCreationInput(req.body);
+    // const { errors, isValid } = validateCreationInput(req.body);
 
     //Check validation
-    if(!isValid) {
-        return res.status(400).json(errors);
-    }
+    // if(!isValid) {
+    //     return res.status(400).json(errors);
+    // }
 
     //Find by name
-    Rally.findOne({ name: req.body.name })
-    .then(rally => {
-        if(rally) {
-          errors.name = 'Rally already exists';
-          return res.status(400).json({errors});
-        }
-        else {
-          const newRally = new Rally({
-            name: req.body.name,
-            owners: req.body.owners.slice(0),
-            members: req.body.members.slice(0),
-            dateExpires: req.body.dateExpires
-          });
+    // Rally.findOne({ name: req.body.name })
+    // .then(rally => {
+    //     if(rally) {
+    //       errors.name = 'Rally already exists';
+    //       return res.status(400).json({errors});
+    //     }
+    //     else {
+    //       const newRally = new Rally({
+    //         name: req.body.name,
+    //         owners: req.body.owners.slice(0),
+    //         members: req.body.members.slice(0),
+    //         dateExpires: req.body.dateExpires
+    //       });
 
-          newRally.save()
-			.then(rally => res.json(rally))
-			.catch(err => console.log(err));
-        }
-    });
+    //       newRally.save()
+		// 	.then(rally => res.json(rally))
+		// 	.catch(err => console.log(err));
+    //     }
+    // });
 })
 
 
 // @route    GET api/rally/current
 // @desc     Return current rally
 // @access   Private
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-	res.json({
-    	name: req.rally.name,
-    	owners: req.rally.owners,
-    	members: req.rally.members,
-    	dateExpires: req.rally.dateExpires
-    });
-})
+// router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+// 	res.json({
+//     	name: req.rally.name,
+//     	owners: req.rally.owners,
+//     	members: req.rally.members,
+//     	dateExpires: req.rally.dateExpires
+//     });
+// })
 
 // @route    GET api/rally
 // @desc     Return user rally
 // @access   Private
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 	const errors = {};
-
+	
 	Rally.findOne({ user: req.user.id })
 		.then(rally => {
 			if(!rally) {
 				errors.norally = 'There is no rally for this user';
 				return res.status(404).json(errors);
 			}
-			res,json(rally);
+			res.json(rally);
 		})
 		.catch(err => res.status(404).json(err));
 });
@@ -86,15 +86,18 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @desc     Create user rally
 // @access   Private
 router.post('/create', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const error = {};
+	
 	const rallyFields = {};
 	rallyFields.owners = req.user.id;
 	if(req.body.name) rallyFields.name = req.body.name;
 	// if(typeof req.body.members != 'undefined') {
 	// 	rallyFields.members = req.body.members.split(',');
 	// }
-	Rally.findOne({ rally: req.rally.id }.then(rally => {
+
+	Rally.findOne({ user: rallyFields.owners }).then(rally => {
 		if (rally) {
-			//update
+			//update - fix later
 			Rally.findOneAndUpdate(
 			{ rally: req.rally.id },
 			{ $set: rallyFields },
@@ -104,7 +107,7 @@ router.post('/create', passport.authenticate('jwt', { session: false }), (req, r
 			//create
 			new Rally(rallyFields).save().then(rally => res.json(rally));
 		}
-	}))
+	})
 });
 
 module.exports = router;
