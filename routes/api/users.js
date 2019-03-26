c193e944758b48b39c37c4236da1a0e93d91f28f
Christy Welch <christywelch2@10-17-234-238.dynapool.wireless.nyu.edu>
2019-03-25 22:41:42 -0400

    updating users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,10 +45,7 @@ router.post('/register', (req, res) => {
 
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if(err)
-                //.then()
-                .catch(err => console.log(err));
-                //throw err;
+              if(err) throw err;
               newUser.password = hash;
               newUser.save()
                 .then(user => res.json(user))
